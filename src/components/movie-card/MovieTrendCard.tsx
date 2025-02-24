58f064517ee6f0e67a6fbe9488247130b70413c0
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-02-24 23:02:58 +0530

    add

--- src/components/movie-card/MovieTrendCard.tsx
+++ src/components/movie-card/MovieTrendCard.tsx
@@ -1,14 +1,25 @@
+import { useContext } from "react";
 import { movieDataType } from "../../assets/data"
+import { Card, CardContent } from "@mui/material";
+import { MovieContext } from "../../context/movie-context";
 
 interface movieTrendCardProps{
     movie:movieDataType;
 }
 
 function MovieTrendCard({movie}:movieTrendCardProps) {
+    const { dispatch } = useContext(MovieContext);
+
   return (
-    <div>
-      movie trend card
-    </div>
+    <Card elevation={0} style={{ backgroundColor: "transparent" }}>
+    <CardContent style={{ padding: 0, position: "relative", display: "flex" }}>
+      <img
+        src={movie.image}
+        alt=""
+        style={{ width: "400px", height: "300px", borderRadius: "8px" }}
+      />
+    </CardContent>
+  </Card>
   )
 }
 
